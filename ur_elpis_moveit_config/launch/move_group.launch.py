4874278cdffe6e7e03fd179925dc3238035e6173
red-lightning10 <inkredible2599@gmail.com>
2025-12-17 01:12:56 -0500

    update: minor changes to enable moveit task constructor and planning pipelines

--- ur_elpis_moveit_config/launch/move_group.launch.py
+++ ur_elpis_moveit_config/launch/move_group.launch.py
@@ -3,5 +3,15 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_robot_cell", package_name="ur_elpis_moveit_config").to_moveit_configs()
+    moveit_config = (
+        MoveItConfigsBuilder("my_robot_cell", package_name="ur_elpis_moveit_config")
+        .planning_pipelines(pipelines=["ompl", "chomp"])
+        .to_moveit_configs()
+    )
+    # Needed for moveit task constructor
+    moveit_config.move_group_capabilities = {
+        "capabilities": "move_group/ExecuteTaskSolutionCapability",
+        "disable_capabilities": ""
+    }
+    
     return generate_move_group_launch(moveit_config)
--- ur_elpis_moveit_config/launch/moveit_rviz.launch.py
+++ ur_elpis_moveit_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,10 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_robot_cell", package_name="ur_elpis_moveit_config").to_moveit_configs()
+    moveit_config = (
+        MoveItConfigsBuilder("my_robot_cell", package_name="ur_elpis_moveit_config")
+        .robot_description_semantic(file_path="config/my_robot_cell.srdf")
+        .planning_pipelines(pipelines=["ompl", "chomp"])
+        .to_moveit_configs()
+    )
     return generate_moveit_rviz_launch(moveit_config)
